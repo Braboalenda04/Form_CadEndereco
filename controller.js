444f4e6bff522584f8acb9c274e56336f8711917
Gabriel R <r.santos52@alunoescolassociais.org.br>
2023-09-29 11:09:52 -0300

    Fazendo uma funçaão para inderoço no Js

---
+++ controller.js
@@ -0,0 +1,12 @@
+'use strict' // modo retrito 
+// cosumindo API de CEP, do ViaCep
+// https://viacep.com.br/
+
+// Função para limpar o formulário
+const limparFormulário =() =>{
+    document.getElementById('rua').Value ='';
+    document.getElementById('bairro').Value ='';
+    document.getElementById('cidade').Value ='';
+    document.getElementById('estado').Value ='';
+}
+
