09518838238a89fb20e99a0160dd85701a9ccd64
Gabriel R <r.santos52@alunoescolassociais.org.br>
2023-09-29 11:26:33 -0300

    fizemos uma Função de cep e numero no js

--- controller.js
+++ controller.js
@@ -10,3 +10,8 @@ const limparFormulário =() =>{
     document.getElementById('estado').Value ='';
 }
 
+// Verifica se CEP é válido 
+const eNUMRO = (numero) => /^[0-9]+$/.test(numero); // testa número informado com expressão regular 
+const cepValido = (cep) => cep.length == 8 && Enumerator(cep); // Verifica tamanho do cep digite e executa função de validação do cep eNumero
+
+
